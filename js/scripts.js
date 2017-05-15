c9925ab316482577c4a9258e311a03d44b413097
Gideon Kipng'etich <gideonmrndn8@gmail.com>
2017-05-15 15:17:07 +0300

    Issue fixed

--- js/scripts.js
+++ js/scripts.js
@@ -5,36 +5,37 @@
 $(document).ready(function() {
   var result = 0;
   var yourscore = 0;
-  var myarray = [1, 2, 3, 4, 5, 6];
+  var myarray = [0, 2, 3, 4, 5, 6];
   $("input#btnSearch").click(function() {
-
 
     var random1 = myarray[Math.floor(Math.random() * myarray.length)];
 
-    var temp = $("span#message");
     result += random1;
     if (random1 === 1) {
       alert("Sorry you rolled a '1'. Hand over the mouse");
+      result = 0;
     }
-
-
     $("span#message").text(random1);
     $("span#thisRound").text(result);
 
 
     $("input#endTurn").click(function() {
 
-      yourscore += result
+      yourscore += result;
 
       $("span#your-score").text(yourscore);
+      if (yourscore >= 100) {
+        alert("You win Player 1");
+      }
 
+      result = 0;
       $("span#message").empty();
       $("span#thisRound").empty();
     });
   });
   result = 0;
   var result1 = 0;
-  var yourscore1 = 0;
+  var opponentscore = 0;
 
   $("input#btnSearch1").click(function() {
 
@@ -44,19 +45,22 @@ $(document).ready(function() {
     result1 += random2;
     if (random2 === 1) {
       alert("Sorry you rolled a '1'. Hand over the mouse");
-
+      result1 = 0;
     }
     $("span#message1").text(random2);
     $("span#thisRound1").text(result1);
     $("input#endTurn1").click(function() {
 
-      var yourscore = parseInt($("span#opponent-score").val());
+      opponentscore += result1;
 
-      $("span#opponent-score").text(result1);
+      $("span#opponent-score").text(opponentscore);
+      if (opponentscore >= 100) {
+        alert("You win Player 2");
+      }
 
+      result1 = 0;
       $("span#message1").empty();
       $("span#thisRound1").empty();
     });
   });
-  result1 = 0;
 });
